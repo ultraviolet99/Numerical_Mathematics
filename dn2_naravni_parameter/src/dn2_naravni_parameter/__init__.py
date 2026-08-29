d8699367a9902eede960f7e962cc4614d0020f3b
ultraviolet99 <curbyourcoding@gmail.com>
2026-08-29 13:08:15 +0200

    Change to normal signs, since it doesn't display in terminal

--- dn2_naravni_parameter/src/dn2_naravni_parameter/__init__.py
+++ dn2_naravni_parameter/src/dn2_naravni_parameter/__init__.py
@@ -1,4 +1,4 @@
-"""DN2 (18.2.6): naravni parameter krivulje (t³ - t, t² - 1)."""
+"""DN2 (18.2.6): naravni parameter krivulje (t^3 - t, t^2 - 1)."""
 from .param import s, hitrost
 from .quad import gauss_legendre, integral
 from .cheb import cheb_tocke, cheb_koeficienti, clenshaw
